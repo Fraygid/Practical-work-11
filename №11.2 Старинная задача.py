fdc25c3f257367b1a77937253924176b3ad602f6
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-12-15 09:45:18 +0300

    Update №11.2 Старинная задача.py

--- №11.2 Старинная задача.py
+++ №11.2 Старинная задача.py
@@ -1,1 +1,7 @@
-asdas
+print('У вас есть 100 руб. Вы можете на них купить:')
+
+for bull in range(11):
+    for cow in range(21):
+        for calf in range(201):
+            if bull * 10 + cow * 5 + calf * 0.5 == 100:
+                print(f'быков={bull}, коров={cow}, телят={calf}')
